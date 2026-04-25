c7546f2d1818df011ea2b5670db537f3b2dc14b4
schmallan <31siyu@gmail.com>
2026-04-25 10:56:19 -0400

    Alvin. Py

---
+++ alvin.py
@@ -0,0 +1,8 @@
+i = input("Who are you? ")
+i = i.trim().lowercase()
+if i == "finn":
+    print("too ugly, access denied")
+
+for i in range(9000):
+    print("I hate windows")
+    print("Finn sucks")
