0166c2665cd5042e93393c6f06180cdebcb978f4
rohan74 <rbhamidipati-28@peddie.org>
2026-04-25 11:07:50 -0400

    backend util file

---
+++ backend/ParseUtil.java
@@ -0,0 +1,35 @@
+import java.io.File;
+
+public class ParseUtil {
+
+    public static int patientBP(File rawText) {
+        return 0;
+    }
+
+    // assign some attribute value to pulse/DBP/SBP
+    public static int patientReadingsBP(File rawText, int day, int attribute) {
+        return 0;
+    }
+
+    // Activity notes
+    public static File patientNotesBP(File rawText) {
+        return new File("googoogaagaa");
+    }
+
+    public static File kidneysRadiology(File rawText) {
+        return new File("googoogaagaa");
+    }
+
+    public static File bladderRadiology(File rawText) {
+        return new File("googoogaagaa");
+    }
+
+    public static File uterusRadiology(File rawText) {
+        return new File("googoogaagaa");
+    }
+
+    public static File patientNotesRadiology(File rawText) {
+        return new File("googoogaagaa");
+    }
+
+}
