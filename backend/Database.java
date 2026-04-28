24f477c9891cebd408fc92e9970d5a76ea123299
rohan74 <rbhamidipati-28@peddie.org>
2026-04-28 15:04:31 -0400

    sql database now on github
    To alan and diya: open the connection in mysql wokrbench (download workbench) to see the database & its tables. i still have to add stuff so it's not done, beware. gitignore is for security purposes

--- backend/Database.java
+++ backend/Database.java
@@ -1,10 +1,37 @@
 import java.sql.*;
 
 public class Database {
-    
 
-    public void addPatient(Patient p) {
-        // send query
+    private Connection database_connection;
+
+    public Database() throws SQLException {
+        final String url = "jdbc:mysql://localhost:3306/patient_database";
+        final String user = Utils.db_user;
+        final String pwd = Utils.db_pwd;
+
+        database_connection = DriverManager.getConnection(url, user, pwd);
+    }
+
+    public void addPatient(Patient p) throws SQLException {
+
+        // create query
+        final String query = "INSERT INTO attributes VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+        PreparedStatement sql = database_connection.prepareStatement(query);
+
+        sql.setString(1, p.full_name); // indexing starts at 1 for jdbc
+        sql.setInt(2, p.patient_id);
+        sql.setString(3, p.date_of_birth);
+        sql.setInt(4, p.sex);
+        sql.setString(5, p.allergies);
+        sql.setString(6, p.existing_medications);
+        sql.setString(7, p.phone_number);
+        sql.setString(8, p.address);
+        sql.setString(9, p.primary_provider);
+        sql.setString(10, p.insurance);
+        sql.setString(11, p.documents);
+        sql.setInt(12, p.hipaa_agreement ? 1 : 0);
+
+        sql.executeUpdate();
     }
 
     public Patient findPatientID(int ID) {
---
+++ patient_database.sql
@@ -0,0 +1,17 @@
+INSERT INTO attributes
+VALUES (
+	"Alan",
+    1,
+    "1927-01-01",
+    1,
+    "N/A",
+    "N/A",
+    "123-456-7890",
+    "1 Main St",
+    "Dr. Doctor",
+    "Insurance Co.",
+    "N/A",
+    1
+);
+
+SELECT * FROM attributes;
