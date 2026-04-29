555ec9a8a55b8d6e66ee149f9f469f50853695e8
schmallan <31siyu@gmail.com>
2026-04-29 13:24:39 -0400

    ;

--- Test.java
+++ Test.java
@@ -2,8 +2,6 @@ import java.sql.*;
 
 public class Test {
     public static void main(String[] args) {
-        for(int i = 0; i < Integer.MAX_VALUE; i++) {
-            System.out.println("didy call finn stinks");
-        }
+        
     }
 }
