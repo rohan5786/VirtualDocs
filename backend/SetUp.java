ff08b4c63e80a9b44357b56b4848f97e3163f46d
rohan74 <rbhamidipati-28@peddie.org>
2026-05-06 12:17:27 -0400

    bub

--- backend/SetUp.java
+++
@@ -1,9 +0,0 @@
-import java.sql.ResultSet;
-
-public class SetUp {
-    public SetUp() {
-        // PreparedStatement stmt = database_connection.createStatement();
-        // ResultSet rs = stmt.executeQuery("SELECT * FROM users");
-
-    }
-}
